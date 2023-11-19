b12a509139ba28d20e80d6c10d5fdfe97e96ea24
Zhitong LIU <zhitonggm.liu@gmail.com>
2023-11-19 18:02:56 +0100

    feat: add system to move paddle

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use bevy::{prelude::*, math::*};
 const PADDLE_START_Y: f32 = 0.0;
 const PADDLE_SIZE: Vec2 = Vec2::new(120.0, 20.0);
 const PADDLE_COLOR: Color = Color::rgb(0.3, 0.3, 0.7);
+const PADDLE_SPEED: f32 = 500.0;
 
 fn main() {
     // println!("Hello, world!");
@@ -14,6 +15,7 @@ fn main() {
         .insert_resource(ClearColor(Color::rgb(0.9, 0.9, 0.9))) // set background color
         .add_systems(Update, bevy::window::close_on_esc) // exit on ESC
         .add_systems(Startup, setup) // call only once when the game starts
+        .add_systems(FixedUpdate, move_paddle) //
         .run()
 }
 
@@ -42,4 +44,25 @@ fn setup(mut commands: Commands) {
             Paddle,
     ));
 
+}
+
+fn move_paddle(
+    input: Res<Input<KeyCode>>,
+    time_step: Res<Time<Fixed>>,
+    mut query: Query<&mut Transform, With<Paddle>>,
+) {
+    let mut paddle_transform = query.single_mut(); // get the only mutable transform by the
+                                                   // "query", if multiple transform matche, it will panic
+
+    let mut direction = 0.0;
+    if input.pressed(KeyCode::A) {
+        direction -= 1.0;
+    }
+    if input.pressed(KeyCode::D) {
+        direction += 1.0;
+    }
+
+    let new_x = paddle_transform.translation.x + direction * PADDLE_SPEED * time_step.delta_seconds(); // time_step.period.as_sec_f32() is 1 frame's time
+
+    paddle_transform.translation.x = new_x;
 }
