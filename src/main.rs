bc3d3744e633869949a7cb8e6fbba16b70036576
Zhitong LIU <zhitonggm.liu@gmail.com>
2023-11-19 16:50:36 +0100

    feat: init game app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+// include the prelude module to get all the basic bevy modules
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins) // include basic plugins like logger, window, input, etc.
+        .add_systems(Update, bevy::window::close_on_esc) // exit on ESC
+        .run()
 }
